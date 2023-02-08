c344e04b040f5b1a5b017929abfc2dbd043d2789
BorbelyBalazs <blaze82bass@gmail.com>
2023-02-08 18:42:14 +0100

    add basic structure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello");
+
+
+        MyLinkedList mList = new MyLinkedList();
+
+        //Add nodes to the list
+        mList.addNode(1);
+        mList.addNode(2);
+        mList.addNode(3);
+        mList.addNode(4);
+
+        //Displays the nodes present in the list
+        mList.display();
+
     }
 }
